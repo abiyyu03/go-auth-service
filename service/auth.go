5f41c6b984b5347cd7edecd72a293fc40d1e41af
abiyyu03 <abiyyu652@gmail.com>
2025-06-05 09:19:34 +0700

    fix(auth): add mechanism password check and store/delete new accesstoken

--- service/auth.go
+++ service/auth.go
@@ -24,14 +24,20 @@ func NewAuthService(repo *repository.UserRepo) AuthServiceInterface {
 }
 
 func (service *AuthService) Login(ctx *fiber.Ctx, auth *dto.AuthRequest) (resp *dto.AuthResponse, err error) {
-	user, err := service.Repo.FetchLogin(auth.Email)
 
+	user, err := service.Repo.FetchLogin(auth.Email)
 	if user == nil {
 		err = fiber.NewError(fiber.StatusUnauthorized, "Invalid email or password")
 		return nil, err
 	}
 	if err != nil {
 		err = fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
+		return nil, err
+	}
+
+	checkPassword, _ := utils.CheckHashPassword(user.Password, auth.Password)
+	if checkPassword == false {
+		err = fiber.NewError(fiber.StatusUnauthorized, "Invalid email or password")
 		return nil, err
 	}
 
@@ -42,7 +48,7 @@ func (service *AuthService) Login(ctx *fiber.Ctx, auth *dto.AuthRequest) (resp *
 		err = fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
 		return nil, err
 	}
-	decryptAccess, err := utils.VerifyJWT(accessToken)
+	decryptAccess, err := utils.ParseJWT(accessToken)
 
 	if err != nil {
 		err = fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
@@ -51,7 +57,7 @@ func (service *AuthService) Login(ctx *fiber.Ctx, auth *dto.AuthRequest) (resp *
 
 	// create refresh token
 	refreshToken, err := utils.CreateJWT("refresh", user.Email, user.Fullname, user.Role.ID)
-	decryptRefresh, err := utils.VerifyJWT(refreshToken)
+	decryptRefresh, err := utils.ParseJWT(refreshToken)
 
 	if err != nil {
 		err = fiber.NewError(fiber.StatusInternalServerError, "Internal server error")
@@ -92,6 +98,82 @@ func (service *AuthService) Login(ctx *fiber.Ctx, auth *dto.AuthRequest) (resp *
 			RefreshToken:   refreshToken,
 			RefreshExpires: decryptRefresh.ExpiresAt.Unix(),
 		},
+	}
+
+	return
+}
+
+func (service *AuthService) RefreshTokenRequest(ctx *fiber.Ctx, refreshToken string) (resp *dto.TokenResponse, err error) {
+	claims, err := utils.ParseJWT(refreshToken)
+	if err != nil {
+		err = fiber.NewError(fiber.StatusUnauthorized, "Invalid refresh token")
+		return nil, err
+	}
+
+	// Create a new access token
+	newAccessToken, err := utils.CreateJWT("access", claims.Email, claims.Fullname, int(claims.RoleID))
+	if err != nil {
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to create new access token")
+		return nil, err
+	}
+
+	decryptAccess, err := utils.ParseJWT(newAccessToken)
+	if err != nil {
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to parse new access token")
+		return nil, err
+	}
+
+	newRefreshToken, err := utils.CreateJWT("refresh", claims.Email, claims.Fullname, int(claims.RoleID))
+	if err != nil {
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to create new refresh token")
+		return nil, err
+	}
+	decryptRefresh, err := utils.ParseJWT(newRefreshToken)
+	if err != nil {
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to parse new refresh token")
+		return nil, err
+	}
+
+	// Get the old auth token from the database
+	oldAuthToken, err := service.AuthTokenRepo.GetAuthTokenByUserID(refreshToken)
+	if err != nil {
+		if err.Error() == "record not found" {
+			err = fiber.NewError(fiber.StatusUnauthorized, "Refresh token not found")
+			return nil, err
+		}
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to get old auth token")
+		return nil, err
+	}
+
+	// remove old auth token
+	err = service.AuthTokenRepo.DeleteAuthTokenByUserID(refreshToken)
+	if err != nil {
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to delete old auth token")
+		return nil, err
+	}
+	// store new auth token in database
+	tokenData := &model.AuthToken{
+		UserID:         oldAuthToken.UserID,
+		AccessToken:    newAccessToken,
+		AccessExpires:  decryptAccess.ExpiresAt.String(),
+		RefreshToken:   newRefreshToken,
+		RefreshExpires: decryptRefresh.ExpiresAt.String(),
+		Revoked:        false,
+		Expired:        false,
+		IPAddress:      ctx.IP(),
+		Device:         ctx.Get("User-Agent"),
+	}
+	err = service.AuthTokenRepo.CreateAuthToken(tokenData)
+	if err != nil {
+		err = fiber.NewError(fiber.StatusInternalServerError, "Failed to create new auth token")
+		return nil, err
+	}
+
+	resp = &dto.TokenResponse{
+		AccessToken:    newAccessToken,
+		AccessExpires:  decryptAccess.ExpiresAt.Unix(),
+		RefreshToken:   newRefreshToken,
+		RefreshExpires: decryptRefresh.ExpiresAt.Unix(),
 	}
 
 	return
